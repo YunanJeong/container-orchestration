e07c295f886c81613d7b98cc5666d0532be9c023
yunanjeong <njhgle@gmail.com>
2024-04-05 16:10:06 +0900

    installer

---
+++ kubernetes/install/install_controlplane.sh
@@ -0,0 +1,23 @@
+# K3s controlplane
+curl -sfL https://get.k3s.io | sh -s - --docker
+
+# K3s kubeconfig 및 bashrc
+mkdir -p ~/.kube
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+# kubectl 설정
+echo 'export KUBECONFIG=~/.kube/config' >> ~/.bashrc
+echo 'alias k="kubectl"' >> ~/.bashrc
+source ~/.bashrc
+
+# helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
+
+# k9s
+sudo apt install -y jq
+K9S_VERSION=$(curl -s https://api.github.com/repos/derailed/k9s/releases/latest | jq -r '.tag_name')
+curl -sL https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_Linux_amd64.tar.gz | sudo tar xfz - -C /usr/local/bin k9s
