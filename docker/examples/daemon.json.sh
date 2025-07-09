aab0c7f93a60baa13595cf0e2b6777a0832eaad6
yunanjeong <njhgle@gmail.com>
2025-07-09 15:48:31 +0900

    log issue

--- docker/examples/daemon.json.sh
+++ docker/examples/daemon.json.sh
@@ -1,13 +1,16 @@
 # 
 # /etc/docker/daemon.json  # Registry and Logging settings  
 # 
+
+# 로그 설정 없을시 disk full
+# k3s와 함께 사용시 로그max-size는 반드시 10m 고정(로그파일로테이션 오류확률 감소)
 URL_DOCKER=${URL_DOCKER:-"docker.wai"}  # If not set, default used
 cat <<EOF > /tmp/daemon.json
 {
   "log-driver": "json-file",
   "log-opts": {
-    "max-size": "200m",
-    "max-file": "5"
+    "max-size": "10m",
+    "max-file": "100"
   },
   "insecure-registries": ["${URL_DOCKER}"],
   "registry-mirrors": ["http://${URL_DOCKER}"]
