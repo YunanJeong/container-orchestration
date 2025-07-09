7e2eb2b5ba89d07177915237f3f19544e53717d4
yunanjeong <njhgle@gmail.com>
2025-07-09 16:49:23 +0900

    mod

--- docker/examples/daemon.json.sh
+++ docker/examples/daemon.json.sh
@@ -3,7 +3,7 @@
 # 
 
 # 로그 설정 없을시 disk full 가능성 있음
-# docker-k3s연동시 로그 max-size는 10m 권장(k3s 기본값과 동일하게)
+# docker-k3s연동시 로그 max-size는 10m 권장(kubelet 기본값)
 # docker와 k3s의 max-size가 다를시, docker 설정대로 로그가 정상처리되지만, 불필요한 에러메시지가 지속 발생함
   # https://github.com/rancher/rancher/issues/39819#issuecomment-1472278470
 URL_DOCKER=${URL_DOCKER:-"docker.wai"}  # If not set, default used
