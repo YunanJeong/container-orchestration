abba2c687966595e55eadb9f60cc8b43f1993851
yunanjeong <njhgle@gmail.com>
2025-09-18 12:10:44 +0900

    command -v

--- kubernetes/install/install_cron_k3s_cert_rotate.sh
+++ kubernetes/install/install_cron_k3s_cert_rotate.sh
@@ -3,7 +3,7 @@
 # K3s Leaf 인증서(1년)을 자동 갱신하기 위한 크론탭을 "등록해주는" 스크립트
 
 # k3s-server: 매월 1일 03:15에 rotate + restart  # 갱신된 kubeconfig 파일권한 수정(편의상 넣었으나, client측 책임이므로 별도 관리하는게 원칙상 좋음)
-SERVER_CMD="15 3 1 * * $(command -v k3s) certificate rotate && $(command -v systemctl) restart k3s  &&  chmod 644 /etc/rancher/k3s/k3s.yaml"
+SERVER_CMD="15 3 1 * * $(command -v k3s) certificate rotate && $(command -v systemctl) restart k3s  && $(command -v chmod) 644 /etc/rancher/k3s/k3s.yaml"
 
 # k3s-agent: 매월 1일 03:25에 restart
 AGENT_CMD="25 3 1 * * $(command -v systemctl) restart k3s-agent"
