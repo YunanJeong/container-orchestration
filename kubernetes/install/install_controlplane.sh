0124d4e9614a9fc50a2d91e18852beac6285a716
yunanjeong <njhgle@gmail.com>
2025-09-19 14:39:40 +0900

    update

--- kubernetes/install/install_controlplane.sh
+++ kubernetes/install/install_controlplane.sh
@@ -2,14 +2,13 @@
 curl -sfL https://get.k3s.io | sh -s - --docker
 
 # K3s settings (Context and etc.)
-# # k3s context파일(k3s.yaml)을 표준경로(~/.kube/config)로 옮겨준다.
-# # 표준경로에 기존 context가 있다면, 수동으로 텍스트 편집하여 k3s 추가 
-# # context 파일의 소유자, 소유그룹이 실제 작업 user와 일치하면서 `chmod 600`을 하면 warning, permission denied 없이 사용가능
-mkdir -p ~/.kube
-sudo cat /etc/rancher/k3s/k3s.yaml > ~/.kube/config
-# sudo chown myuser:myuser ~/.kube/config
-sudo chmod 600 ~/.kube/config
-echo 'export KUBECONFIG=~/.kube/config' >> ~/.bashrc
+# # kubeconfig(context 파일)의 소유자, 소유그룹이 실제 작업 user와 일치하면서 `chmod 600`을 하면 warning, permission denied 없이 사용가능
+# mkdir -p ~/.kube  # KUBECONFIG=$HOME/.kube/config
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+sudo chmod 600 $KUBECONFIG  
+sudo chown $USER:$USER $KUBECONFIG
+
+echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> ~/.bashrc
 echo 'alias k="kubectl"' >> ~/.bashrc
 source ~/.bashrc
 
